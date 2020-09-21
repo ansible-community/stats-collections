46fb961a3f9c64faa387d417718b758791c315fc
Greg Sutcliffe <github@emeraldreverie.org>
2020-09-21 15:12:12 +0100

    Initial mockups of time-to-close UI via {shinipsum}

--- R/app_server.R
+++ R/app_server.R
@@ -1,10 +1,43 @@
 #' The application server-side
-#' 
-#' @param input,output,session Internal parameters for {shiny}. 
+#'
+#' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
-#' @import shiny
+#' @import shiny shinipsum ggplot2
+#' @importFrom ggplot2 labs
 #' @noRd
 app_server <- function( input, output, session ) {
-  # List the first level callModules here
 
+  output$survival_plot <- renderPlot({
+    repo <- input$repo
+    random_ggplot() + labs(title = repo)
+  })
+
+  output$timeseries_plot <- renderPlot({
+    repo <- input$repo
+    random_ggplot('point')
+  })
+
+  output$bar_plot <- renderPlot({
+    repo <- input$repo
+    random_ggplot('bar')
+  })
+
+  output$summary_table <- renderTable({
+    random_table(5,3)
+  })
+
+  output$timeSinceLastUpdate <- renderUI({
+    # Get this from the DB later
+    p(
+      class = "text-muted",
+      "Data refreshed",
+      round(digits = 1,
+        difftime(
+          Sys.time(),
+          file.mtime('crawler/ansible-collections%community.general/issues.json'),
+          units="hours")
+      ),
+      " hours ago."
+    )
+  })
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -1,34 +1,99 @@
 #' The application User-Interface
-#' 
-#' @param request Internal parameter for `{shiny}`. 
+#'
+#' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
-#' @import shiny
+#' @import shiny shinydashboard
+#' @importFrom shinydashboard dashboardSidebar
 #' @noRd
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # List the first level UI elements here 
-    fluidPage(
-      h1("ansible.collections")
+    # List the first level UI elements here
+    dashboardPage(
+      ui_header(),
+      dashboardSidebar(disable = TRUE),
+      ui_body()
+    )
+  )
+}
+
+#' @import shinydashboard
+#' @importFrom shinydashboard dashboardHeader
+#' @noRd
+ui_header <- function() {
+  dashboardHeader(title = "Ansible Collections")
+}
+
+#' @import shinydashboard
+#' @importFrom shinydashboard dashboardBody
+#' @noRd
+ui_body <- function() {
+  dashboardBody(
+    fluidRow(
+      column(width = 10,
+             box(width = NULL, solidHeader = TRUE,
+                 plotOutput("survival_plot", height = 600)
+             ),
+             column(width = 4,
+                    box(width = NULL,
+                        tableOutput("summary_table")
+                    )
+             ),
+             column(width = 4,
+                    box(width = NULL,
+                        plotOutput("timeseries_plot", height = 250)
+                    )
+             ),
+             column(width = 4,
+                    box(width = NULL,
+                        plotOutput("bar_plot", height = 250)
+                    )
+             )
+      ),
+      column(width = 2,
+             box(width = NULL, status = "warning",
+                 selectInput("graph_type", "Metric",
+                             choices = c(
+                               'Time-to-close Issues/PRs' = 'ttclose'
+                             ),
+                             selected = 'ttclose'
+                 ),
+             ),
+             box(width = NULL, status = "warning",
+                 selectInput("repo", "Collection",
+                             choices = c(
+                               'community.general',
+                               'grafana',
+                               'ios'
+                             ),
+                             selected = "grafana"
+                 ),
+                 uiOutput("timeSinceLastUpdate"),
+                 p(class = "text-muted",
+                   br(),
+                   "Source data updates daily."
+                 )
+             )
+      )
     )
   )
 }
 
 #' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
 #' @import shiny
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function(){
-  
+
   add_resource_path(
     'www', app_sys('app/www')
   )
- 
+
   tags$head(
     favicon(),
     bundle_resources(
@@ -36,7 +101,6 @@ golem_add_external_resources <- function(){
       app_title = 'ansible.collections'
     )
     # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+    # for example, you can add shinyalert::useShinyalert()
   )
 }
-
